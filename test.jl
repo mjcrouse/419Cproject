2f3176d2710fde10be16b877d5f91cdf053dc879
Michael Crouse <michaeljcrouse@gmail.com>
2020-03-29 19:51:33 -0700

    Update test.jl

--- test.jl
+++ test.jl
@@ -1,6 +1,17 @@
-using LinearAlgebra
+#using LinearAlgebra
 
 dag = [0 1 1 0 0 0 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 1 0 1 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 1 0 1; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 0 0 0 0 0]
+
+#TODO connect names of nodes to dag
+A = [.5 .5]
+B = [.5 .5;.4 .6]
+C = [.7 .3;.2 .8]
+D = [.9 .1;.5 .5]
+E = [.3 .7;.6 .4]
+F = [.01 .99;.01 .99; .01 .99; .99 .01]
+G = [.8 .2; .1 .9]
+H = [.05 .95; .95 .05; .95 .05; .95 .05]
+
 println("dag")
 println(dag)
 
@@ -10,35 +21,86 @@ if n != size(dag)[2]
     error("dag must be an N x N matrix")
 end
 
-sdag = dag
+ug = deepcopy(dag)
 #remove direction of graph (make dag symmetric)
 for m = 1:n-1
     for i = m+1:n
-            if sdag[m,i] == 1
-                sdag[i,m] = 1
+            if ug[m,i] == 1
+                ug[i,m] = 1
             end
     end
 end
 
-println(sdag)
+println("undirected graph")
+println(ug)
+
+mg = deepcopy(ug)
 
 # connect parents to finish moral graph
 for m = 3:n
     for i = 1:m-2
         for j = i+1:m-1
-            if sdag[m,i] == 1 && sdag[m,j] == 1
-                sdag[i,j] = 1
-                sdag[j,i] = 1
+            if mg[m,i] == 1 && mg[m,j] == 1
+                mg[i,j] = 1
+                mg[j,i] = 1
             end
         end
     end
 end
 println("moral graph")
-println(sdag)
+println(mg)
 
-nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-println(nodes)
+#nodes = ["A", "B", 'C', 'D', 'E', 'F', 'G', 'H']
+#println(nodes)
 
-#check without weights for now
-#function check(dag)
+f = []
+clusters = []
+#check without weights for now (get fewest edges added)
+function check(g) #input moral graph
+    f = Int8[]
+    a = zeros(Int8,n)
+    for m = 1:n
+        b = Int8[]
+        cl = [m]
+        counter = 0
+        for i = 1:n
+            if g[m,i] == 1
+                push!(b,i)
+                push!(cl,i)
+            end
+            #println(b)
+        end
+        #println(b)
+        #println(cl)
+        push!(clusters,cl)
+        if (l = length(b)) > 1
+            for x = 1:l
+                for y = x+1:l
+                    if g[(b[x]),(b[y])] == 0
+                        counter =+ 1
+                    end
+                end
+            end
+        end
+        a[m] = counter;
+    end
+    #println(a) 
+    #f = []
+    min = minimum(a)
+    for k in 1:length(a)
+        if ( min == a[k])
+            push!(f,k)
+        end
+    end
+    #println(clusters)
+    return f
+end
+
+f = check(mg)
+println("lowest\n", f)
+
+function weight(nd) #input node, return weight of corresponding node cluster
+    
+end
+
 
